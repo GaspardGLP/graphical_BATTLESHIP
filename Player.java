7f9a7f2ac25d217fcef9cdd6f25b2e7bdd89e8ad
GLPgaspard <gaspard.grignet-le-perron@epitech.eu>
2025-01-07 22:51:57 +0100

    [FIX] Fixing the boat importation issue

--- Player.java
+++ Player.java
@@ -1,7 +1,4 @@
 import javax.swing.*;
-import java.awt.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Player {
     private final Board board;
@@ -11,7 +8,7 @@ public class Player {
     }
 
     public boolean placeShip(int row, int col, int size, boolean horizontal, JButton[][] grid) {
-        return board.placeShip(row, col, size, horizontal, grid);
+        return board.placeShip(row, col, size, horizontal, grid, true);
     }
 
     public boolean attack(int row, int col, JButton[][] grid, Player opponent) {
