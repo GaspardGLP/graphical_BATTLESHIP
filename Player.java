ea76a154e5d7770eb4f099011270b17d2f457297
unknown <gaspardglp@gmail.com>
2025-01-07 11:14:08 +0100

    [MAJ] New player class

--- Player.java
+++ Player.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class Player {
+    private String name;
+    private Board board;
+
+    public Player(String name) {
+        this.name = name;
+        this.board = new Board();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Board getBoard() {
+        return board;
+    }
 }
