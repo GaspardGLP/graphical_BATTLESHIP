3a1d6eeea52d404ba0317815cae07b1ad5c92045
unknown <gaspardglp@gmail.com>
2025-01-07 11:11:28 +0100

    [MAJ] Changing the main class to make it easier

---
+++ BattleShipGameGUI.java
@@ -0,0 +1,7 @@
+public class BattleShipGameGUI {
+    public static void main(String[] args) {
+        // Initialisation du jeu
+        GameWindow gameWindow = new GameWindow();
+        gameWindow.startGame();
+    }
+}
