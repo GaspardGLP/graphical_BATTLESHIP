0d803fb761b2f3f1c5dd724a89155c4ad7996689
GLPgaspard <gaspard.grignet-le-perron@epitech.eu>
2025-01-07 22:59:16 +0100

    [ADD] Add commentary to understand the code

--- BattleShipGameGUI.java
+++ BattleShipGameGUI.java
@@ -1,7 +1,9 @@
 import javax.swing.*;
 
+// Main class to launch the Battleship game GUI
 public class BattleShipGameGUI {
     public static void main(String[] args) {
+        // Run the GameWindow class on the Swing event dispatch thread
         SwingUtilities.invokeLater(GameWindow::new);
     }
 }
